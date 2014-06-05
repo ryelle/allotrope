5595e79f20366650c7703a37e62e2e73025601e3
ryelle <kelly.dwan@10up.com>
2014-06-05 12:54:22 -0400

    Add setup function, which adds assorted theme support (most importantly, post-thumbails)

--- functions.php
+++ functions.php
@@ -16,6 +16,51 @@ if ( ! isset( $content_width ) ) {
 }
 
 /**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function allotrope_setup() {
+
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 */
+	load_theme_textdomain( 'allotrope', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 * See http://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside', 'image', 'gallery', 'video', 'quote', 'link', 'chat', 'audio', 'status'
+	) );
+
+}
+add_action( 'after_setup_theme', 'allotrope_setup' );
+
+
+/**
  * Enqueue scripts and styles.
  *  'wp-api-js' handles the Backbone models, views, etc.
  *  'app-init' initializes the app, handles data display.
