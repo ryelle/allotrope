83f22727ac66be989c0b65518b319c0435f06fc3
ryelle <kelly.dwan@gmail.com>
2014-08-14 11:54:05 -0400

    Remove commented out code

--- app/js/init.js
+++ app/js/init.js
@@ -100,33 +100,4 @@
 
 	wp.api.app.start();
 
-
-	// var App = Backbone.Router.extend({
-
-	// 	routes: {
-	// 		'':                   'default',
-	// 		'post/:id':           'single',
-	// 		'post/:id/*slug':     'single'
-	// 	},
-
-	// 	default: function() {
-	// 		var index = new wp.api.views.Index();
-	// 		$('#content').html( index.render().el );
-	// 	},
-
-	// 	single: function( id ) {
-	// 		var single = new wp.api.views.Single({
-	// 			model: new wp.api.models.Post({ ID: id })
-	// 		});
-	// 		if ( ! $('#content .single-post').length ){
-	// 			$('#content').prepend( '<div class="single-post"></div>' );
-	// 		}
-	// 		$('#content .single-post').hide();
-	// 		$('#content .single-post').html( single.render().el );
-	// 		$('#content .single-post').css({ top: ( $( window ).scrollTop() + 10 ) + 'px' });
-	// 	}
-	// });
-
-	// Backbone.history.start();
-
 })( wp, jQuery, window );
