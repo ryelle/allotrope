4c67bafb254e0980e0b51f5d0780a2255c111879
ryelle <kelly.dwan@gmail.com>
2014-08-17 16:57:24 -0400

    Clean up routing and add docs

--- app/js/init.js
+++ app/js/init.js
@@ -37,16 +37,72 @@
 	};
 
 	_.extend(wp.api.app.Controller.prototype, {
-		// Start the app by showing the appropriate views
-		// and fetching the list of todo items, if there are any
+		// Start the app by showing the global views
 		start: function () {
-			// this.posts.fetch();
+			this.showDecoration();
 		},
 
+		/**
+		 * Default route handler, show all posts.
+		 */
+		default: function () {
+			this.showPosts( this.posts, {} );
+			this.showPagination();
+		},
+
+		/**
+		 * Load posts from a specific category
+		 *
+		 * @param {integer|string}  category  ID or slug of category to display
+		 */
+		category: function ( category ) {
+			if ( typeof category === 'string' ) {
+				this.showPosts( this.posts, { 'filter[category_name]': category } );
+			} else {
+				this.showPosts( this.posts, { 'filter[cat]': category } );
+			}
+			this.showPagination();
+		},
+
+		/**
+		 * Load posts from a specific tag
+		 *
+		 * @param {string}  tag  slug of tag to display
+		 */
+		tag: function ( tag ) {
+			this.showPosts( this.posts, { 'filter[tag]': tag } );
+			this.showPagination();
+		},
+
+		/**
+		 * Load posts that match a search term
+		 *
+		 * @param {string}  query  search query string
+		 */
+		search: function ( query ) {
+			this.showPosts( this.posts, { 'filter[s]': query } );
+			this.showPagination();
+		},
+
+		/**
+		 * Load a single post
+		 *
+		 * @param {integer}  id  post ID of post to display
+		 */
+		single: function( id ){
+			this.showPost( id );
+		},
+
+		/**
+		 * Load the pagination into the pagination region
+		 */
 		showPagination: function(){
 			wp.api.app.pagination.show( new wp.api.views.Pagination() );
 		},
 
+		/**
+		 * Attach decorative diamonds to the main region
+		 */
 		showDecoration: function(){
 			wp.api.app.main.$el.append( wp.template( 'background' )({ size:250, class: 'small' }) );
 			wp.api.app.main.$el.append( wp.template( 'background' )({ size:350, class: 'medium' }) );
@@ -54,6 +110,12 @@
 			wp.api.app.main.$el.append( wp.template( 'background' )({ size:950, class: 'very-large' }) );
 		},
 
+		/**
+		 * Load the posts requested into the main region
+		 *
+		 * @param {object}  posts  Collection of Posts to be passed into the Index view
+		 * @param {object}  data   Map of queries for this page, so we can filter further in the view
+		 */
 		showPosts: function ( posts, data ) {
 			wp.api.app.main.show(new wp.api.views.Index({
 				collection: posts,
@@ -61,42 +123,15 @@
 			}));
 		},
 
+		/**
+		 * Load a single post into the single region
+		 *
+		 * @param {integer}  id  Post ID of the post to display
+		 */
 		showPost: function( id ) {
 			wp.api.app.single.show(new wp.api.views.Single({
 				model: new wp.api.models.Post({ ID: id })
 			}));
-		},
-
-		default: function () {
-			this.showPosts( this.posts, {} );
-			this.showPagination();
-			this.showDecoration();
-		},
-
-		// Category expects an integer ID
-		category: function ( category ) {
-			this.showPosts( this.posts, { 'filter[cat]': category } );
-			this.showPagination();
-			this.showDecoration();
-		},
-
-		// Tag expects a string slug
-		tag: function ( tag ) {
-			this.showPosts( this.posts, { 'filter[tag]': tag } );
-			this.showPagination();
-			this.showDecoration();
-		},
-
-		// Search expects a string query
-		search: function ( query ) {
-			this.showPosts( this.posts, { 'filter[s]': query } );
-			this.showPagination();
-			this.showDecoration();
-		},
-
-		single: function( id ){
-			this.showPost( id );
-			this.showDecoration();
 		}
 	});
 
@@ -119,6 +154,9 @@
 		Backbone.history.start();
 	});
 
+	/**
+	 * Add in support for Backbone's navigation functionality
+	 */
 	wp.api.app.navigate = function( fragment, options ){
 		Backbone.history.navigate( fragment, options );
 	}
